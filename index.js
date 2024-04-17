ea6cf1e547c1db82b441990c8a00a084256e2e3d
La Ngoc Hai <langochai11121998@gmail.com>
2024-04-17 10:19:18 +0700

    set up basic project

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ToDo = require("./models/ToDoModel");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use()
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
@@ -15,11 +15,20 @@ mongoose
 	.then(() => console.log("Connected to MongoDB"))
 	.catch((err) => console.error("Could not connect to MongoDB", err));
 
-app.get("/", (req, res) => {
-	res.render("index");
+app.get("/", async (req, res) => {
+	const data = await ToDo.find();
+	res.render("index", { data });
 });
-app.post("/", (req, res) => {console.log(req.body);});
-app.delete("/", (req, res) => {});
+app.post("/", async (req, res) => {
+	const newToDo = new ToDo({
+		Data: req.body.value,
+	});
+	await newToDo.save();
+});
+app.delete("/:data", async (req, res) => {
+	const { data } = req.params;
+	ToDo.findOneAndDelete({ Data: data });
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
--- models/ToDoModel.js
+++ models/ToDoModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 const ToDoSchema = new mongoose.Schema({ Data: String });
-const ToDo = mongoose.model("ToDo", ToDoSchema);
+const ToDo = mongoose.model("ToDo", ToDoSchema, "to-do");
 
 module.exports = ToDo;
